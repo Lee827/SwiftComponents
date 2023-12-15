543e86c0cb76ef6741cf97d02b1ebdc9c1ccda02
Elmo Lee <>
2023-12-15 16:33:30 +0800

    Create Framework

---
+++ SwiftComponents/SwiftComponents.h
@@ -0,0 +1,18 @@
+//
+//  SwiftComponents.h
+//  SwiftComponents
+//
+//  Created by KWUN LOK LEE on 15/12/2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftComponents.
+FOUNDATION_EXPORT double SwiftComponentsVersionNumber;
+
+//! Project version string for SwiftComponents.
+FOUNDATION_EXPORT const unsigned char SwiftComponentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftComponents/PublicHeader.h>
+
+
